e58845ad6e8bce725d36db9bebcfb189737c175b
ViacheslavAnisimov <forvsn@gmail.com>
2023-03-08 03:53:59 +0700

    edit kata

--- src/kyu7/ch4113ng3.js
+++ src/kyu7/ch4113ng3.js
@@ -1,5 +1,5 @@
 // Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
 
 function nerdify(txt){
-  return txt.replace(/[aA]/g, "4").replace(/[eE]/g, "3").replace(/[l]/g, "1");
+  return txt.replace(/[a]/gi, "4").replace(/[e]/gi, "3").replace(/[l]/g, "1");
 }
